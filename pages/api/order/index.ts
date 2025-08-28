914802451c9d65757ae3d2c7bb001f289ef102b2
Sebastian <campo.sebastian44@gmail.com>
2025-08-27 22:02:20 -0300

    algolia connection

--- pages/api/order/index.ts
+++ pages/api/order/index.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import * as methods from "micro-method-router";
+import fs from "../../../lib/firebase";
 
 export default methods({
     async post(req: NextApiRequest, res: NextApiResponse){
         var queryParams = req.query;
         res.send({
-            response: "Ok desde /order POST",
+            response: "test",
             query: {queryParams}
         });
     }
--- pages/api/sync/index.ts
+++ pages/api/sync/index.ts
@@ -1,28 +1,30 @@
-// import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next"
+import * as methods from "micro-method-router";
+import { syncData } from "../../../lib/algolia";
 
-// import * as methods from "micro-method-router";
-
-// export default methods({
-//     async post(req: NextApiRequest, res: NextApiResponse){
-//         var queryParams = req.query;
-//         airtableBase('Furniture').select({}).eachPage(async function page(records, fetchNextPage) {
-//             // This function (`page`) will get called for each page of records.
-//             var objects = records.map((r)=>{
-//                 return {
-//                     indexId: r.id,
-//                     ...r.fields,
-//                 }
-//             })
-//             await client.saveObjects({indexName: "test", objects: objects});
-//             console.log(objects)
-//             fetchNextPage();
+export default methods({
+    async post(req: NextApiRequest, res: NextApiResponse){
+        var queryParams = req.query;
+        const sync = await syncData();
+        res.send("ok")
+        // airtableBase('Furniture').select({}).eachPage(async function page(records, fetchNextPage) {
+        //     // This function (`page`) will get called for each page of records.
+        //     var objects = records.map((r)=>{
+        //         return {
+        //             indexId: r.id,
+        //             ...r.fields,
+        //         }
+        //     })
+        //     await client.saveObjects({indexName: "test", objects: objects});
+        //     console.log(objects)
+        //     fetchNextPage();
             
 
-//         }, function done(err) {
-//             if (err) { console.error(err); return; }
-//         });
+        // }, function done(err) {
+        //     if (err) { console.error(err); return; }
+        // });
 
-//         res.send("ok")
+        // res.send("ok")
         
-//     }
-// })
+    }
+})
