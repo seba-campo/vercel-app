e67046228b4ee5298f8d49a667c169a968557d1a
Sebastian Campo <campo.sebastian44@gmail.com>
2025-09-12 19:52:17 -0300

    webhook 2

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -5,11 +5,11 @@ import { messaging } from "firebase-admin";
 
 export default methods({
     async post(req: NextApiRequest, res: NextApiResponse){
-        const body: WebhokPayload = await req.body;
+        const body = await req.body;
         console.log("Webhook received", body);
 
-        if (body.type === "payment") {
-            const mpPayment = await getPaymentById(body.id as any);
+        if (body.topic === "payment") {
+            const mpPayment = await getPaymentById(body.resource as any);
             if (mpPayment.status === "approved") {
                 console.log(`Payment ${mpPayment.id} approved`);
                 const purchaseId = mpPayment.external_reference;
