d5549b2f45df8c6866ee763bb8e4bcdcdb6fc7bd
Sebastian Campo <campo.sebastian44@gmail.com>
2025-09-12 20:01:33 -0300

    modify mercadopago hook

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import * as methods from "micro-method-router";
 import { getPaymentById, WebhokPayload} from "../../../lib/mercadopago";
 import { messaging } from "firebase-admin";
+import { confirmateOrderAsync } from "../../../controllers/mercadopagoController";
 
 export default methods({
     async post(req: NextApiRequest, res: NextApiResponse){
@@ -13,6 +14,7 @@ export default methods({
             if (mpPayment.status === "approved") {
                 console.log(`Payment ${mpPayment.id} approved`);
                 const purchaseId = mpPayment.external_reference;
+                await confirmateOrderAsync(purchaseId);
                 res.send(200)
              }
         }
