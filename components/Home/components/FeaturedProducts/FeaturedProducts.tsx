8762007c887ae602ceee244deebb83e78595c2ad
Sebastian Campo <campo.sebastian44@gmail.com>
2025-12-12 23:51:01 -0300

    feat: Implement featured products section with dynamic product detail pages and styled product cards.

--- components/Home/components/FeaturedProducts/FeaturedProducts.tsx
+++ components/Home/components/FeaturedProducts/FeaturedProducts.tsx
@@ -1,4 +1,39 @@
 import { FeaturedProductsWrapper, FeaturedProductsContainer } from "./FeaturedProductsStyle"
+import Router from "next/router"
+import ProductGrid from "../../../ProductGrid"
+import ProductCard from "../../../ProductCard"
+
+const mockProducts = [{
+    id: 1,
+    name: "Reloj inteligente",
+    price: 79300,
+    imageUrl: "https://via.placeholder.com/150"
+},
+{
+    id: 2,
+    name: "Termo con Bolso",
+    price: 25999,
+    imageUrl: "https://via.placeholder.com/150"
+},
+{
+    id: 3,
+    name: "Ventilador de pie",
+    price: 24000,
+    imageUrl: "https://via.placeholder.com/150"
+},
+{
+    id: 4,
+    name: "Mouse ergonómico inalámbrico",
+    price: 17000,
+    imageUrl: "https://via.placeholder.com/150"
+},
+{
+    id: 5,
+    name: "Reloj Casio",
+    price: 34530,
+    imageUrl: "https://via.placeholder.com/150"
+}]
+
 
 export const FeaturedProducts = () => {
     return (
@@ -6,7 +41,20 @@ export const FeaturedProducts = () => {
             <h1 className="title">Productos Destacados</h1>
 
             <FeaturedProductsContainer>
-
+                <ProductGrid>
+                    {
+                        mockProducts.map(product => (
+                            <ProductCard
+                                key={product.id}
+                                id={product.id.toString()}
+                                name={product.name}
+                                price={product.price}
+                                imageUrl={product.imageUrl}
+                                onClick={() => Router.push(`/product/${product.id}`)}
+                            />
+                        ))
+                    }
+                </ProductGrid>
             </FeaturedProductsContainer>
         </FeaturedProductsWrapper>
     )
